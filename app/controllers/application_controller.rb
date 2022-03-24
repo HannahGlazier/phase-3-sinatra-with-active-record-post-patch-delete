c7ef5401504ef2d45ecc1fad7a368f5cd668dadf
Hannah Glazier <hrayglazier@aol.com>
2022-03-24 16:05:45 -0600

    commit tree 8809792c32a0a879a46d39e4c4ea55e2709d7d7c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < Sinatra::Base
+  require 'pry'
   set :default_content_type, 'application/json'
 
   get '/games' do
@@ -14,6 +15,31 @@ class ApplicationController < Sinatra::Base
         user: { only: [:name] }
       } }
     })
+  end
+
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  post "/reviews" do 
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
   end
 
 end
